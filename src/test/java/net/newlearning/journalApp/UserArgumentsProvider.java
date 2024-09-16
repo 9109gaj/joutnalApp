1f997ebb4260e83ef9d630f40f3e249d62a0fe8a
9109gaj <rgpatidar9109866407@gmail.com>
2024-09-16 20:34:54 +0530

    sonar Lint properties

--- src/test/java/net/newlearning/journalApp/UserArgumentsProvider.java
+++ src/test/java/net/newlearning/journalApp/UserArgumentsProvider.java
@@ -1,22 +1,22 @@
-package net.newlearning.journalApp;
-
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.extension.ExtensionContext;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.ArgumentsProvider;
-import org.springframework.security.core.userdetails.User;
-import java.util.stream.Stream;
-
-@Disabled
-public class UserArgumentsProvider  implements   ArgumentsProvider{
-
-
-    @Override
-    public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) throws Exception {
-        return  Stream.of(
-                Arguments.of(User.builder().username("shyam").password("shyam").build()),
-                Arguments.of(User.builder().username("ram").password("ram").build())
-
-        );
-    }
-}
+//package net.newlearning.journalApp;
+//
+//import org.junit.jupiter.api.Disabled;
+//import org.junit.jupiter.api.extension.ExtensionContext;
+//import org.junit.jupiter.params.provider.Arguments;
+//import org.junit.jupiter.params.provider.ArgumentsProvider;
+//import org.springframework.security.core.userdetails.User;
+//import java.util.stream.Stream;
+//
+//@Disabled
+//public class UserArgumentsProvider  implements   ArgumentsProvider{
+//
+//
+//    @Override
+//    public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) throws Exception {
+//        return  Stream.of(
+//                Arguments.of(User.builder().username("shyam").password("shyam").build()),
+//                Arguments.of(User.builder().username("ram").password("ram").build())
+//
+//        );
+//    }
+//}
--- src/test/java/net/newlearning/journalApp/UserServicesTests.java
+++ src/test/java/net/newlearning/journalApp/UserServicesTests.java
@@ -1,41 +1,41 @@
-package net.newlearning.journalApp;
-
-import net.newlearning.journalApp.Entity.User;
-import net.newlearning.journalApp.Repository.UserRepository;
-import net.newlearning.journalApp.service.UserServices;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ArgumentsSource;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-@Disabled
-@SpringBootTest
-public class UserServicesTests {
-
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private UserServices userServices;
-
-    @ParameterizedTest
-    @ArgumentsSource(UserArgumentsProvider.class)
-    public void testSaveNewUser(User  user){
-        assertTrue(userServices.saveNewUser(user));
-
-    }
-
-    @Disabled
-@ParameterizedTest
-    @CsvSource({"1,2,3",
-            "8,9,90"
-    })
-    public void tests(int a, int b, int expected){
-        assertEquals(expected , a +b);
-    }
-}
+//package net.newlearning.journalApp;
+//
+//import net.newlearning.journalApp.Entity.User;
+//import net.newlearning.journalApp.Repository.UserRepository;
+//import net.newlearning.journalApp.service.UserServices;
+//import org.junit.jupiter.api.Disabled;
+//import org.junit.jupiter.params.ParameterizedTest;
+//import org.junit.jupiter.params.provider.ArgumentsSource;
+//import org.junit.jupiter.params.provider.CsvSource;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@Disabled
+//@SpringBootTest
+//public class UserServicesTests {
+//
+//
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    @Autowired
+//    private UserServices userServices;
+//
+//    @ParameterizedTest
+//    @ArgumentsSource(UserArgumentsProvider.class)
+//    public void testSaveNewUser(User  user){
+//        assertTrue(userServices.saveNewUser(user));
+//
+//    }
+//
+//    @Disabled
+//@ParameterizedTest
+//    @CsvSource({"1,2,3",
+//            "8,9,90"
+//    })
+//    public void tests(int a, int b, int expected){
+//        assertEquals(expected , a +b);
+//    }
+//}
